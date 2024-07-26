5b563f4ee7bbb05be078dfebb1df5238dc6534a3
k5-inoue <k5-inoue@bandainamco-mirai.com>
2024-07-26 17:19:12 +0900

    Linux更新

--- src/build/build_linux.bash
+++ src/build/build_linux.bash
@@ -2,6 +2,7 @@
 
 SRC_DIR=".."
 OUT_DIR="./bin/linux"
+LIB_DIR="hts_engine_API-linux-x64-static_lib"
 
 # for clean build
 rm -rf ${OUT_DIR}/*
@@ -11,3 +12,10 @@ cmake -S ../ -G "Unix Makefiles" -B ${OUT_DIR} \
     -D CMAKE_BUILD_TYPE=Release
 
 cmake --build ${OUT_DIR} --config Release -j 4
+
+# collect lib & header
+mkdir -p ${OUT_DIR}/${LIB_DIR}/{lib,include}
+cp ${OUT_DIR}/lib/libhts_engine_API.a ${OUT_DIR}/${LIB_DIR}/lib/
+cp ${SRC_DIR}/include/HTS_engine.h ${OUT_DIR}/${LIB_DIR}/include/
+
+echo "Build fin. --> ${OUT_DIR}/${LIB_DIR}"
